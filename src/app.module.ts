58d5fe98c9cff428d6cc30b4dd81c4ac762d90dc
imran-binhasan <imranbinhasan.work@gmail.com>
2025-08-13 12:46:14 +0600

    integrate orm & database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal:true,
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+     imports:[ConfigModule],
+     inject:[ConfigService],
+     useFactory:(configService:ConfigService) => ({
+      type:'postgres',
+      host:configService.get<string>('DB_HOST'),
+      port:configService.get<number>('DB_PORT'),
+      username:configService.get<string>('DB_USER'),
+      password:configService.get<string>('DB_PASSWORD'),
+      database:configService.get<string>('DB_NAME'),
+      synchronize:configService.get<string>('NODE_ENV')=='development',
+      autoLoadEntities:true,
+      ssl:{rejectUnauthorized:false}
+     })
+    })],
   controllers: [AppController],
   providers: [AppService, { provide: APP_FILTER, useClass: HttpExceptionFilter }],
 })
