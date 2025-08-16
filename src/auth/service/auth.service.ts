612b5ea6b5bdf35f83212a56d8b73ffac9cd337a
imran-binhasan <imranbinhasan.work@gmail.com>
2025-08-16 09:01:47 +0600

    add auth-service

---
+++ src/auth/service/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { JwtPayload } from "../guard/jwt.strategy";
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private jwtService:JwtService
+    ){}
+
+   generateToken(payload:JwtPayload):string{
+    return this.jwtService.sign(payload, {expiresIn: '12h'})
+   }
+
+   verifyToken(token:string):JwtPayload{
+    return this.jwtService.verify<JwtPayload>(token)
+   }
+}
