421b840027585f33b4bd6044868a3066262e0e16
imran-binhasan <imranbinhasan.work@gmail.com>
2025-08-17 09:36:28 +0600

    init role, permission, auth , admin module & add in main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,10 @@ import { HttpExceptionFilter } from './common/filter/http-exception.filter';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ResponseInterceptor } from './common/interceptor/response.interceptor';
+import { AuthModule } from './auth/module/auth.module';
+import { AdminModule } from './admin/module/admin.module';
+import { RoleModule } from './role/module/role.module';
+import { PermissionModule } from './permission/module/permission.module';
 
 @Module({
   imports: [
@@ -13,21 +17,26 @@ import { ResponseInterceptor } from './common/interceptor/response.interceptor';
       isGlobal: true,
     }),
     TypeOrmModule.forRootAsync({
-     imports:[ConfigModule],
-     inject:[ConfigService],
-     useFactory:(configService:ConfigService) => ({
-      type:'postgres',
-      host:configService.get<string>('DB_HOST'),
-      port:configService.get<number>('DB_PORT'),
-      username:configService.get<string>('DB_USER'),
-      password:configService.get<string>('DB_PASSWORD'),
-      database:configService.get<string>('DB_NAME'),
-      synchronize:configService.get<string>('NODE_ENV')=='development',
-      autoLoadEntities:true,
-      ssl:{rejectUnauthorized:false}
-     })
-    })],
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        synchronize: configService.get<string>('NODE_ENV') == 'development',
+        autoLoadEntities: true,
+        ssl: { rejectUnauthorized: false }
+      })
+    }),
+    AuthModule,
+    AdminModule,
+    RoleModule,
+    PermissionModule
+  ],
   controllers: [AppController],
-  providers: [AppService, { provide: APP_FILTER, useClass: HttpExceptionFilter }, {provide: APP_INTERCEPTOR, useClass:ResponseInterceptor}],
+  providers: [AppService, { provide: APP_FILTER, useClass: HttpExceptionFilter }, { provide: APP_INTERCEPTOR, useClass: ResponseInterceptor }],
 })
-export class AppModule {}
+export class AppModule { }
