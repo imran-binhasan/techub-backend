23022aa54dffd271c690338051bc612c2e98d17e
imran-binhasan <imranbinhasan.work@gmail.com>
2025-08-17 08:12:43 +0600

    add upload module

---
+++ src/upload/module/upload.module.ts
@@ -0,0 +1,10 @@
+import { Module } from "@nestjs/common";
+import { CloudinaryService } from "../service/cloudinary.service";
+
+@Module({
+    imports:[],
+    providers:[CloudinaryService],
+    exports:[CloudinaryService]
+})
+
+export class UploadModule{}
