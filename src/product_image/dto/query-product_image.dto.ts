5fab3c2a90bf53f7a36900f75a8c4d9c98e9e339
imran-binhasan <imranbinhasan.work@gmail.com>
2025-08-31 05:19:45 +0600

    add product_image query dto

---
+++ src/product_image/dto/query-product_image.dto.ts
@@ -0,0 +1,18 @@
+import { IsOptional, IsUUID, IsBoolean } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { PaginationQuery } from 'src/common/dto/pagination_query.dto';
+
+export class ProductImageQueryDto extends PaginationQuery {
+    @IsOptional()
+    @IsUUID()
+    productId?: string;
+
+    @IsOptional()
+    @Transform(({ value }) => {
+        if (value === 'true') return true;
+        if (value === 'false') return false;
+        return undefined;
+    })
+    @IsBoolean()
+    isPrimary?: boolean;
+}
